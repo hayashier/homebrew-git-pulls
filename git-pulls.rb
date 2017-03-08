ce1c6aeca6d49614afa39d0bf35b3b75a9bed88a
shogo <goodheaven700@gmail.com>
2017-03-08 18:21:17 +0900

    modify url path in formula

--- git-pulls.rb
+++ git-pulls.rb
@@ -4,7 +4,7 @@ HOMEBREW_GITPULLS_VERSION='0.0.1'
 class GitPulls < Formula
   desc "git-pulls command pulls all git repository under specified paths."
   homepage "#{REPOSITORY_URL}/README.md"
-  url "#{REPOSITORY_URL}/bin/git-pulls"
+  url "#{REPOSITORY_URL}/blob/master/bin/git-pulls"
   sha256 "8c73c2e5325ec7d6568dc4129770e59f2e9d46898f86c9f147420ec4d2760c39"
   head "#{REPOSITORY_URL}.git", :tag => "#{HOMEBREW_GITPULLS_VERSION}"
   version "#{HOMEBREW_GITPULLS_VERSION}"
