b2d5674faf09b0689c4fcb31951aa4a868b9e7a5
shogo <goodheaven700@gmail.com>
2017-03-08 17:13:23 +0900

    add Formula

---
+++ homebrew-git-pulls.rb
@@ -0,0 +1,31 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class HomebrewGitPulls < Formula
+  desc "git-pulls command pulls all git repository under specified paths."
+  homepage "https://github.com/hayashier/homebrew-git-pulls/README.md"
+  head "https://github.com/hayashier/homebrew-git-pulls.git"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    bin.install "bin/git-pulls"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test homebrew-git-pulls`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
