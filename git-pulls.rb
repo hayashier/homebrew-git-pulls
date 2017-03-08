788a65364827b316aeca4882cd524e895edd84ce
shogo <goodheaven700@gmail.com>
2017-03-08 18:14:39 +0900

    add hash to Formula

--- git-pulls.rb
+++ git-pulls.rb
@@ -1,8 +1,13 @@
+REPOSITORY_URL='https://github.com/hayashier/homebrew-git-pulls'
+HOMEBREW_GITPULLS_VERSION='0.0.1'
+
 class GitPulls < Formula
   desc "git-pulls command pulls all git repository under specified paths."
-  homepage "https://github.com/hayashier/homebrew-git-pulls/README.md"
-  head "https://github.com/hayashier/homebrew-git-pulls.git"
-  version "0.0.1"
+  homepage "#{REPOSITORY_URL}/README.md"
+  url "#{REPOSITORY_URL}/bin/git-pulls"
+  sha256 "8c73c2e5325ec7d6568dc4129770e59f2e9d46898f86c9f147420ec4d2760c39"
+  head "#{REPOSITORY_URL}.git", :tag => "#{HOMEBREW_GITPULLS_VERSION}"
+  version "#{HOMEBREW_GITPULLS_VERSION}"
 
   def install
     bin.install "bin/git-pulls"
