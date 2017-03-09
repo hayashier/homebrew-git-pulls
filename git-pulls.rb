fcf9776ac28917cbaf83b2e0be016c05585aeb56
shogo <goodheaven700@gmail.com>
2017-03-09 14:26:54 +0900

    change hash

--- git-pulls.rb
+++ git-pulls.rb
@@ -5,7 +5,7 @@ class GitPulls < Formula
   desc "Pulls all git repository under specified paths."
   homepage "#{REPOSITORY_URL}/README.md"
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
-  sha256 "e30e011045596bcd704fe20f606f75c7cd44159d7809217bac7e33bac0843cd2"
+  sha256 "d76fafba595af1f7a66648788277cba3b5c4628aa37ad92dcda50392132cc103"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
