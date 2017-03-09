9bca2e2b11e5c0344252e9c674b3c44369fa5e07
shogo <goodheaven700@gmail.com>
2017-03-09 14:16:17 +0900

    modify formula formatly

--- git-pulls.rb
+++ git-pulls.rb
@@ -1,13 +1,13 @@
-REPOSITORY_URL='https://github.com/hayashier/homebrew-git-pulls'
-HOMEBREW_GITPULLS_VERSION='0.0.3'
+REPOSITORY_URL="https://github.com/hayashier/homebrew-git-pulls".freeze
+HOMEBREW_GITPULLS_VERSION="0.0.3".freeze
 
 class GitPulls < Formula
   desc "git-pulls command pulls all git repository under specified paths."
   homepage "#{REPOSITORY_URL}/README.md"
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
   sha256 "ed4c4b760e8aa5c1a95d3eedb957237085817fddeb094135ce899b7edacd0316"
-  head "#{REPOSITORY_URL}.git", :tag => "#{HOMEBREW_GITPULLS_VERSION}"
-  version "#{HOMEBREW_GITPULLS_VERSION}"
+  head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
+  version HOMEBREW_GITPULLS_VERSION
 
   def install
     bin.install "bin/git-pulls"
