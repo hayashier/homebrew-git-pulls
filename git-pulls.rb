f65c8c07777a17e3a04b7d6c79a07611a6d8a947
shogo <goodheaven700@gmail.com>
2017-03-09 14:22:18 +0900

    enable to pass audit

--- git-pulls.rb
+++ git-pulls.rb
@@ -2,12 +2,11 @@ REPOSITORY_URL="https://github.com/hayashier/homebrew-git-pulls".freeze
 HOMEBREW_GITPULLS_VERSION="0.0.3".freeze
 
 class GitPulls < Formula
-  desc "git-pulls command pulls all git repository under specified paths."
+  desc "Pulls all git repository under specified paths."
   homepage "#{REPOSITORY_URL}/README.md"
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
   sha256 "e30e011045596bcd704fe20f606f75c7cd44159d7809217bac7e33bac0843cd2"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
-  version HOMEBREW_GITPULLS_VERSION
 
   def install
     bin.install "bin/git-pulls"
