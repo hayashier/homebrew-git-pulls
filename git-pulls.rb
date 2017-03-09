67b2ebd614dc93591689004c164006919479d2bb
shogo <goodheaven700@gmail.com>
2017-03-09 14:41:09 +0900

    momdify url in formula

--- git-pulls.rb
+++ git-pulls.rb
@@ -3,7 +3,7 @@ HOMEBREW_GITPULLS_VERSION="0.0.4".freeze
 
 class GitPulls < Formula
   desc "Pulls all git repository under specified paths."
-  homepage "#{REPOSITORY_URL}/README.md"
+  homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
   sha256 "d76fafba595af1f7a66648788277cba3b5c4628aa37ad92dcda50392132cc103"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
