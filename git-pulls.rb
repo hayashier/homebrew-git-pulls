6aefc8893c253951e73a14793b4c587d78ea9db3
shogo <goodheaven700@gmail.com>
2017-03-17 01:00:55 +0900

    change directory structure

--- git-pulls.rb
+++ git-pulls.rb
@@ -1,11 +1,11 @@
-REPOSITORY_URL="https://github.com/hayashier/homebrew-git-pulls".freeze
-HOMEBREW_GITPULLS_VERSION="0.0.5".freeze
+REPOSITORY_URL="https://github.com/hayashier/git-pulls".freeze
+HOMEBREW_GITPULLS_VERSION="0.0.1".freeze
 
 class GitPulls < Formula
   desc "Pulls all git repository under specified paths."
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
-  sha256 "90b57b61fdbf52ad2edba440260dab97be65b3c5aae8d02f27b20ae8aa1dfcef"
+  sha256 "ed3b6451a05567505bd718db8fdf343f47e97d70fc7d4d92e7f5cb3c9532b753"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
